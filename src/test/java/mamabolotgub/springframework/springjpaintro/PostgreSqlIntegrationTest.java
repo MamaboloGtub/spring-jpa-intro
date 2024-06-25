f010932735bd415eeed74a39df2e02c533786dd3
Tshepo Mamabolo Mahudu <tmmmmadi@gmail.com>
2024-06-25 15:36:48 +0200

    tests

---
+++ src/test/java/mamabolotgub/springframework/springjpaintro/PostgreSqlIntegrationTest.java
@@ -0,0 +1,32 @@
+package mamabolotgub.springframework.springjpaintro;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ActiveProfiles;
+
+import mamabolotgub.springframework.springjpaintro.repositories.BookRepository;
+
+/**
+ * Tshepo M Mahudu
+ */
+@ActiveProfiles("local")
+@DataJpaTest
+@ComponentScan(basePackages = {"mamabolotgub.springframework.springjpaintro.bootstrap"})
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class PostgreSqlIntegrationTest {
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    void testPostgreSQL() {
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(3);
+    }
+
+}
